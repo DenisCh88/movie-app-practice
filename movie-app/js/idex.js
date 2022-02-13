6c9daaeb8113cae62d74e034608a23af1b247d19
Денис Чухліб <dchuhleb@icloud.com>
2022-02-13 14:19:55 +0200

    feat: add search functions

--- movie-app/js/idex.js
+++ movie-app/js/idex.js
@@ -5,8 +5,13 @@ const movieImage = document.querySelectorAll('.movie__img');
 const movieHeader = document.querySelectorAll('.movie__header');
 const movieVote = document.querySelectorAll('.movie__vote');
 const movieOverview = document.querySelectorAll('.movie__overview');
+const input = document.querySelector('.header__search');
+const form = document.querySelector('.header__form');
+
+
 const page = Math.floor(Math.random() * 500) + 1;
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=4e862687ced12b29e07204eec8aa9590&page=${page}`;
+
 
 const getData = async () => {
 	const res = await fetch(url);
@@ -30,10 +35,37 @@ for(let i = 0; i < movieImage.length; i++){
 	}
  }
 
-
-
  setTimeout(getData, 0);
 
+ const getDataInput = async () => {
+	const urls = `https://api.themoviedb.org/3/search/movie?query=${input.value}&api_key=4e862687ced12b29e07204eec8aa9590`;
+	const res = await fetch(urls);
+	const data = await res.json();
+	const results = data.results;
+	const imgUrl = results.map(el => el.poster_path);
+	const title = results.map(el => el.title);
+	const vote = results.map(el => el.vote_average);
+	const overview = results.map(el => el.overview);
+input.value = '';
+	console.log(results);
+	console.log(input.value)
+	console.log(urls)
 
- 
+for(let i = 0; i < movieImage.length; i++){
+	const img = document.createElement('img');
+	img.classList.add('img__movie');
+	img.src = `https://image.tmdb.org/t/p/w1280${imgUrl[i]}`;
+	img.alt = `image`;
+	movieImage[i].append(img)
+	movieHeader[i].innerHTML = title[i];
+	movieVote[i].innerHTML = vote[i];
+	movieOverview[i].innerHTML = `Overview <br/> <br/> ${overview[i]}`;
+	}
+ }
 
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	getDataInput();
+	
+  })
+ //https://api.themoviedb.org/3/search/movie?query=summer&api_key=4e862687ced12b29e07204eec8aa9590
